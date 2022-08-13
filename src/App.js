87cd5433a055b8862ee4b180369af081a7eda5bb
wumbles <77227394+wumbles@users.noreply.github.com>
2022-08-13 02:23:29 -0600

    setup the foundation for the workshop

--- src/App.js
+++ src/App.js
@@ -2,9 +2,22 @@ import React from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import { Container, Navbar, NavbarBrand } from "reactstrap";
+import NucampLogo from "./app/assets/img/logo.png";
 
 function App() {
-  return <div className="App">hello nucamp!</div>;
+  return (
+    <div className="App">
+      <Navbar dark color="primary" sticky="top" expand="md">
+        <Container>
+          <NavbarBrand href="/">
+            <img src={NucampLogo} alt="nucamp logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      I'm ready for workshop!
+    </div>
+  );
 }
 
 export default App;
