e2c3b4663992e180e78388faa74ce0bc9ab82e52
wumbles <77227394+wumbles@users.noreply.github.com>
2022-08-24 17:33:11 -0600

    CampsitesDirectoryPage and CampsiteDetail

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
-import logo from "./logo.svg";
-import { Counter } from "./features/counter/Counter";
+import { CampsitesDirectoryPage } from "./pages/CampsitesDirectoryPage";
 import "./App.css";
-import { CampsitesList } from "./features/campsites/CampsitesList";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <CampsitesList />
+      <CampsitesDirectoryPage />
       <Footer />
     </div>
   );
