dc4453b6f5fbbbb0e103392843ccf3a5cf7e13c1
wumbles <77227394+wumbles@users.noreply.github.com>
2022-08-24 20:13:10 -0600

    CampsiteDetailPage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Footer } from "./components/Footer";
 import { CampsitesDirectoryPage } from "./pages/CampsitesDirectoryPage";
 import { HomePage } from "./pages/HomePage";
 import { ContactPage } from "./pages/ContactPage";
+import { campsiteDetailPage } from "./pages/CampsiteDetailPage";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="contact" element={<ContactPage />} />
         <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="directory/:campsiteId" element={<campsiteDetailPage />} />
       </Routes>
       <Footer />
     </div>
