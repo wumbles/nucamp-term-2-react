c4756fd8a8e9895685ba954a54e830f443794335
wumbles <77227394+wumbles@users.noreply.github.com>
2022-08-27 20:21:08 -0600

    task 2 of workshop week 2 complete

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { CampsitesDirectoryPage } from "./pages/CampsitesDirectoryPage";
 import { HomePage } from "./pages/HomePage";
 import { ContactPage } from "./pages/ContactPage";
 import { CampsiteDetailPage } from "./pages/CampsiteDetailPage";
+import { AboutPage } from "./pages/AboutPage";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -14,6 +15,7 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
         <Route path="contact" element={<ContactPage />} />
         <Route path="directory" element={<CampsitesDirectoryPage />} />
         <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
