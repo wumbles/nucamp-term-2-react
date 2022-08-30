84657b1c208359cdc04f00de781862a3240476f0
wumbles <77227394+wumbles@users.noreply.github.com>
2022-08-30 01:20:45 -0600

    Revert "Comments added to campsites"
    This reverts commit 0edffe9b92b8f95462ff1fc5abee1014cf9d84da.

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Footer } from "./components/Footer";
 import { CampsitesDirectoryPage } from "./pages/CampsitesDirectoryPage";
 import { HomePage } from "./pages/HomePage";
 import { ContactPage } from "./pages/ContactPage";
-import { CampsiteDetailPage } from "./pages/CampsiteDetailPage";
+import { campsiteDetailPage } from "./pages/CampsiteDetailPage";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,7 +16,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="contact" element={<ContactPage />} />
         <Route path="directory" element={<CampsitesDirectoryPage />} />
-        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+        <Route path="directory/:campsiteId" element={<campsiteDetailPage />} />
       </Routes>
       <Footer />
     </div>
