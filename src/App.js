d0c4333c2904f9c545b80032319267b973476d06
wumbles <77227394+wumbles@users.noreply.github.com>
2022-08-30 20:07:57 -0600

    post workshop 2 files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import { CampsitesDirectoryPage } from "./pages/CampsitesDirectoryPage";
-import { HomePage } from "./pages/HomePage";
-import { ContactPage } from "./pages/ContactPage";
-import { campsiteDetailPage } from "./pages/CampsiteDetailPage";
-import { Routes, Route } from "react-router-dom";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from 'react-router-dom';
+import ContactPage from './pages/ContactPage';
+import HomePage from './pages/HomePage';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
+import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="contact" element={<ContactPage />} />
-        <Route path="directory" element={<CampsitesDirectoryPage />} />
-        <Route path="directory/:campsiteId" element={<campsiteDetailPage />} />
-      </Routes>
-      <Footer />
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <Routes>
+                <Route path='/' element={<HomePage />} />
+                <Route path='contact' element={<ContactPage />} />
+                <Route path='about' element={<AboutPage />} />
+                <Route path='directory' element={<CampsitesDirectoryPage />} />
+                <Route
+                    path='directory/:campsiteId'
+                    element={<CampsiteDetailPage />}
+                />
+            </Routes>
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
