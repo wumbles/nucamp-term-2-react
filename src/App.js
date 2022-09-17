9b4a9832dc8278d586ea78f6446b49dee5fb326e
wumbles <77227394+wumbles@users.noreply.github.com>
2022-09-16 20:14:01 -0600

    Async campsites

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import { Routes, Route } from 'react-router-dom';
-import ContactPage from './pages/ContactPage';
-import HomePage from './pages/HomePage';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
-import CampsiteDetailPage from './pages/CampsiteDetailPage';
-import AboutPage from './pages/AboutPage';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import ContactPage from "./pages/ContactPage";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
+import AboutPage from "./pages/AboutPage";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchCampsites } from "./features/campsites/campsitesSlice";
+import "./App.css";
 
 function App() {
-    return (
-        <div className='App'>
-            <Header />
-            <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='contact' element={<ContactPage />} />
-                <Route path='about' element={<AboutPage />} />
-                <Route path='directory' element={<CampsitesDirectoryPage />} />
-                <Route
-                    path='directory/:campsiteId'
-                    element={<CampsiteDetailPage />}
-                />
-            </Routes>
-            <Footer />
-        </div>
-    );
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchCampsites());
+  }, [dispatch]);
+
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="contact" element={<ContactPage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
